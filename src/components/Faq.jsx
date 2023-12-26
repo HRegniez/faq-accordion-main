e8e4ac1502230837191c5d7504de8e2fe0dc71ac
HRegniez <henriregniez124@gmail.com>
2023-12-26 11:51:09 +0100

    added basic logic to the faq, having trouble animating,

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,13 +1,14 @@
-
+import { useState } from "react"
 function Faq({question, response}) {
+    const [faqState, setFaqState] = useState(false)
     
   return (
     <li>
-        <div className="faq_question">
+        <div className="faq_question" onClick={() => setFaqState(!faqState)}>
           <h2>{question}</h2>
-          <img src="../images/icon-plus.svg" alt="more info icon" />
+          <img src={!faqState ? "../images/icon-plus.svg" : "../images/icon-minus.svg"} alt="more info icon" />
         </div>
-        <div>
+        <div className={`faq_response ${!faqState ? "" : "open"}`}>
           <p>
             {response}
           </p>
